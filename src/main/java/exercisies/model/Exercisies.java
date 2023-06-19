a8d9b46bd6e69d20d29fea99af4fc47ae5705337
kswelder <welderhayne2000@gmail.com>
2023-06-18 22:30:20 -0300

    Add lombok and remove anothers methods

---
+++ src/main/java/exercisies/model/Exercisies.java
@@ -0,0 +1,17 @@
+package exercisies.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Exercisies {
+    private String name;
+    private List<String> exercisies;
+}
